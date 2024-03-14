615a4fd43adae720b29f2de4baedaf51475f2a6e
Santiago Coria <santiago_coria@hotmail.com>
2024-03-14 11:29:18 -0300

    Preparedness Task4

--- src/com/amazon/ata/deliveringonourpromise/Shell.java
+++ src/com/amazon/ata/deliveringonourpromise/Shell.java
@@ -37,7 +37,7 @@ public class Shell {
     private ATAUserHandler inputHandler;
 
     // FIXME: Added to cause a problem with Spotbug
-    private String unusedPrivateString;
+    /*private String unusedPrivateString;*/
 
     /**
      * Constructs a Shell instance that will use the given service client.
